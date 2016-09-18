dc1377bced5f60aad6c43a3c96e41a7b9ef4b542
ericywl <midnight2903@gmail.com>
2016-09-19 02:22:23 +0800

    changes to problem 5

--- problem 5.py
+++ problem 5.py
@@ -1,6 +1,3 @@
-# 2 3 4 5 6 7 8 9 10
-# 2 3 2 5 1 7 2 3 1
-
 primes = []
 powers = []
 pro = 1
@@ -32,5 +29,5 @@ for i in range(2, 21):
 
 print(pro)
 
-# 
+# find smallest positive number evenly divisible by 1-20
 
